26113f505d95531fdb1a8d38e7d8090e0ca4f8b4
Nguyễn Tất Vũ <t_nguyen100748@fanshaweonline.ca>
2025-02-24 19:57:39 -0500

    fix jsdom

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
-  testMatch: ['**/__tests__/**/*.test.js'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom']
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  testPathIgnorePatterns: ['/node_modules/']
 };
