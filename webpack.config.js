7b07a8466d7c4b09f3cf8bcd73038bf5879830ba
Nguyễn Tất Vũ <t_nguyen100748@fanshaweonline.ca>
2025-02-24 23:55:33 -0500

    Move back to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '',
     clean: true,
   },
   mode: 'development',
@@ -41,6 +41,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: 'style.css', to: 'style.css' },
+        { from: 'sw.js', to: 'sw.js' }
       ],
     }),
     new webpack.DefinePlugin({
